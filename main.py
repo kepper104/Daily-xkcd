21eae7a127da622f8f05fe77f6da7c1e8ab0e40a
kepper104 <67190416+kepper104@users.noreply.github.com>
2023-12-13 23:36:13 +0300

    Update parser and Message contents

--- main.py
+++ main.py
@@ -8,28 +8,29 @@ RANDOM_COMIC_LINK = "https://c.xkcd.com/random/comic/"
 
 bot = TeleBot(TELEGRAM_API_KEY)
 
+
 def get_comic_data():
     comic_link = r.get(RANDOM_COMIC_LINK).url
     res = r.get(comic_link)
     s = BeautifulSoup(res.content, 'html.parser')
-    res = s.find_all('img')[2]
+    res = s.find_all('img')[1]
     print(comic_link)
-    # print(res.status_code)
-    # print(res['src'])
-    # print(res['title'])
-    # print(res['src'][2:], res['title'])
+    print(res)
+
     number = comic_link.split("/")[-2]
     url = res['src'][2:]
-    title = res['title']
-    return url, title, number
+    title = res['alt']
+    body_text = res["title"]
+    return url, title, number, body_text
+
 
 def send_daily_comic():
-    data = get_comic_data()
-    text = "Good Morning! Your daily comic is " + data[2]
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="-"*50)
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=text)
-    bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=data[0])
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=data[1])
+    url, title, number, body_text = get_comic_data()
+    text = (f"Good Morning! Your daily comic is {number} - {title} \n"
+            f'Dont forget to solve today\'s <a href="https://www.nytimes.com/games/wordle/index.html">Wordle</a>!')
+
+    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=text, parse_mode="HTML", disable_web_page_preview=True)
+    bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=url, caption=body_text)
 
 
 schedule.every().day.at(SENDING_TIME).do(send_daily_comic)
